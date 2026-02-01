e264c465dec9000235b41fb6aca1690e950ff422
Sai Sujith <pvsaisujith@gmail.com>
2026-02-01 16:41:44 +0530

    delete link is functioning by adding removefromcart function

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,10 @@
-export const cart=[{
+export let cart=[{
   productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity:1
 },{
   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity:2
-},
-
-];
+}];
 
 export function addtoCart(productId){
 let matchingItem;
@@ -26,3 +24,12 @@ let matchingItem;
         });
       };
 };
+
+export function removeFromCart(productId){
+  const newCart=[]
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId!==productId)
+      newCart.push(cartItem);
+  });
+  cart=newCart;
+}
