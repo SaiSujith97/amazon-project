32761917000b4705a291333a068829d6cad4b3fa
Sai Sujith <pvsaisujith@gmail.com>
2026-02-03 22:35:35 +0530

    save data and generate HTML for deliveryOptions

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,29 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
   productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:1
+  quantity:1,
+  deliveryOptionId:'1'
 },{
   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity:2
+  quantity:2,
+  deliveryOptionId:'2'
 }];
+}
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function addtoCart(productId){
 let matchingItem;
+
       cart.forEach((item)=>{
         if(productId===item.productId){
           matchingItem=item;         
         }
       });
+      
       if(matchingItem){
         matchingItem.quantity++;
       }
@@ -20,9 +31,12 @@ let matchingItem;
       {
         cart.push({
           productId:productId,
-          quantity:1
+          quantity:1,
+          deliveryOptionId:'1'
         });
       };
+
+      saveToStorage();
 };
 
 export function removeFromCart(productId){
@@ -32,4 +46,6 @@ export function removeFromCart(productId){
       newCart.push(cartItem);
   });
   cart=newCart;
+
+  saveToStorage();
 }
